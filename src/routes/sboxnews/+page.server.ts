3b40920847bad68e418e66910ea6163c7785945d
Kicks <nolandimensions@gmail.com>
2025-02-04 10:43:25 -0500

    Update +page.server.ts

--- src/routes/sboxnews/+page.server.ts
+++ src/routes/sboxnews/+page.server.ts
@@ -5,21 +5,7 @@ import path from 'path';
 export const load: PageServerLoad = async ({ fetch }) => {
     const allPostFiles = fetch( "https://services.facepunch.com/sbox/news/organization/nolankicks" );
     
-    let unsortedPosts: NewsPost[] = await (await allPostFiles).json();
-
-    if ( !unsortedPosts )
-    {
-        return {
-            status: 404,
-            error: new Error( "Posts not found" )
-        };
-    }
-
-    unsortedPosts = unsortedPosts.filter( (post: NewsPost) => post.Sections[0].Contents !== "" );
-
-    unsortedPosts.forEach( (post: NewsPost) => {
-        console.log( post.Sections[0] );
-    });
+    let unsortedPosts: NewsPost[] = []
 
     return { unsortedPosts };
 };
